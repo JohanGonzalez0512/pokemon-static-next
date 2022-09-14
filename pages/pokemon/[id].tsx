98b0c040ac08cea5639d088cf2dece1b23e7a226
Johan Gonzalez <jopi20101@gmail.com>
2022-09-13 19:46:23 -0500

    Incremental static generation added

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -110,7 +110,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         paths: pokemons151.map(id => ({
             params: { id }
         })),
-        fallback: false
+        fallback: 'blocking'
     }
 }
 
@@ -118,11 +118,22 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const id = params?.id as string;
-    
+    const pokemon = await getPokemonInfo(id);
+
+    if (!pokemon) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
     return {
         props: {
-            pokemon: await getPokemonInfo(id)
-        }
+            pokemon
+        },
+        revalidate: 86400
     }
 }
 
--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -4,11 +4,16 @@ import { Pokemon } from "../interfaces";
 
 export const getPokemonInfo = async (pokemonNameOrId: string) => {
 
-    const { data } = await pokeApi.get<Pokemon>(`pokemon/${pokemonNameOrId}`);
+    try {
+        const { data } = await pokeApi.get<Pokemon>(`pokemon/${pokemonNameOrId}`);
 
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites
+        }
+    } catch (error) {
+        return null
     }
+
 }
